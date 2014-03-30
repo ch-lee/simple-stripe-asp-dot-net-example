97e4856655f4b0feeb837c0b5c466bd19a7783e5
chuen hing lee <chuen.hing.lee@gmail.com>
2014-03-30 22:07:31 +0100

    adding simple stripe payment integration with asp.net mvc

---
+++ src/SimpleStripeExample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleStripeExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Payment/
+
+        public ActionResult Index()
+        {
+            return View("Payment");
+        }
+
+        public ActionResult Charge(string stripeToken, string stripeEmail)
+        {
+            string apiKey = "your-test-secret-key";
+            var stripeClient = new Stripe.StripeClient(apiKey);
+
+            dynamic response = stripeClient.CreateChargeWithToken(2500, stripeToken, "GBP", stripeEmail);
+
+            if (response.IsError == false && response.Paid)
+            {
+                // success
+            }
+
+            return View("Payment");
+
+        }
+
+    }
+}
